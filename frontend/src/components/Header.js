94a5be60d005dec4e2ec6730918491208843691c
Phoenix-the <thephoenix1408@gmail.com>
2022-06-25 23:08:48 +0530

    data added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
-						<Navbar.Brand>ShopNow</Navbar.Brand>
+						<Navbar.Brand>Tingle</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
